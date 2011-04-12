a525ea89db0a7f560098d3d5bc0688c9ee318d43
Joey Trapp <joey@loadsys.com>
2011-04-12 14:27:12 -0500

    Added param parsing to jquery plugin

--- jquery.pluginLoader.js
+++ jquery.pluginLoader.js
@@ -6,11 +6,23 @@ var methods = {
 	init: function(options) {
 		return this.each(function(index) {
 			var self = $(this);
+			var attrs = self[0].attributes;
+			for (var i = 0; i < attrs.length; i++) {
+				if (!attrs[i].nodeName.match(/data-(.)*/)) continue;
+				var parsed = attrs[i].nodeName.replace(/data-/, '');
+				if (loader[parsed]) loader[parsed](self, self.data(parsed));
+			}
 		});
 	}
 };
 
-$.fn.pluginLoader = function(options) {
+$.fn.pluginLoader = function(plugins, options) {
+	if (typeof plugins === 'string') {
+		plugins = plugins.split(',').map(function(el) { return el.trim(); });
+	} else if (plugins instanceof Array) {
+		plugins = plugins.map(function(el) { return el.trim(); });
+	}
+	loader = $.extend(loader, options);
 	/*if (methods[options]) {
 		return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
 	} else if (typeof options === 'object' || !options) {
@@ -18,15 +30,6 @@ $.fn.pluginLoader = function(options) {
 	} else {
 		$.error('Method '+options+' does not exist on jQuery.pluginLoader');
 	}*/
-	//$(plugins).each(function(index) {
-	//	var self = $(this);
-	//	var attrs = self[0].attributes;
-	//	for (var i = 0; i < attrs.length; i++) {
-	//		if (!attrs[i].nodeName.match(/data-(.)*/)) continue;
-	//		var parsed = attrs[i].nodeName.replace(/data-/, '');
-	//		if (loader[parsed]) loader[parsed](self, self.data(parsed));
-	//	}
-	//});
 }
 
 })(jQuery);
